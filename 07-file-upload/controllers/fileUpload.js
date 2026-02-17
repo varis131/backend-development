b4715d093d399fee1c05f0e438ee7725d296ce46
Varis <movaris85@gmail.com>
2026-02-18 01:45:38 +0530

    created a image upload handler while learning file upload

--- 07-file-upload/controllers/fileUpload.js
+++ 07-file-upload/controllers/fileUpload.js
@@ -1,22 +1,24 @@
 const File = require("../models/File");
+const cloudinary = require("cloudinary").v2;
 
 //localFileUpload controller (handler)
 exports.localFileUpload = async (req, res) => {
   try {
-    //fetch file from request 
+    //fetch file from request
     const file = req.files.file;
     console.log("file aa gayi oyyee", file);
-    
-    //create path jaha file ko store kRNA HAI ON SERVER 
-    let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`;
+
+    //create path jaha file ko store kRNA HAI ON SERVER
+    let path =
+      __dirname + "/files/" + Date.now() + `.${file.name.split(".")[1]}`;
     console.log("path_name", path);
 
-    //add path to the move function 
+    //add path to the move function
     file.mv(path, (err) => {
       console.log(err);
     });
 
-    //success ka response send kardo 
+    //success ka response send kardo
     res.json({
       success: true,
       message: "local file uploaded successfully.",
@@ -24,4 +26,45 @@ exports.localFileUpload = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+
+
+//function to find wheather a file is supported or not
+function isFileTypeSupported(type, supportedTypes) {
+  return supportedTypes.includes(type);
+}
+
+//function for uploading file to cloudinary
+async function uploadFileToCloudinary(file, folder) {
+  await cloudinary.uploader.upload(file.tempFilePath);
+}
+
+
+// image upload handler
+exports.imageUpload = async (req, res) => {
+  try {
+    //data fetch
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.imageFile;
+    console.log(file);
+
+    //validation
+    const supportedTypes = ["jpeg", "png", "jpg"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    //file format not supported
+    if (!isFileTypeSupported(type, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "file format enot supported.",
+      });
+    }
+
+    //file format supported
+    const response=await uploadFileToCloudinary(file, "cloudinaryFiles");
+
+  } catch (error) {}
 };
